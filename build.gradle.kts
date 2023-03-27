c5d7ad69556bb90d24c7712a7bd8d7d49991d48d
Harsh3305 <harshverma3305@gmail.com>
2023-03-27 23:05:05 +0530

    Minor change

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
     mavenCentral()
     maven {
         name = "GitHubPackages"
-        url = uri("https://maven.pkg.github.com/hrv-mart/product-library")
+        url = uri("https://maven.pkg.github.com/hrv-mart/user-library")
         credentials {
             username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
             password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
