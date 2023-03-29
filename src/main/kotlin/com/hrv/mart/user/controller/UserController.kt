00b86c1a83e6cc36fa1336cf61800b3feace68e0
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-29 15:56:28 +0530

    Added kafka support (#41)

--- src/main/kotlin/com/hrv/mart/user/controller/UserController.kt
+++ src/main/kotlin/com/hrv/mart/user/controller/UserController.kt
@@ -1,12 +1,14 @@
 package com.hrv.mart.user.controller
 
 import com.hrv.mart.user.service.UserService
-import com.hrv.mart.userlibrary.User
+import com.hrv.mart.userlibrary.model.User
+import com.hrv.mart.userlibrary.model.UserTopics
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.kafka.annotation.KafkaListener
+
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.switchIfEmpty
 
@@ -17,24 +19,25 @@ class UserController (
     private val userService: UserService
 )
 {
-    @PostMapping
-    fun createUser(@RequestBody user: User, response: ServerHttpResponse) =
+    @KafkaListener(
+        topics = [UserTopics.createUserTopic],
+        groupId = "\${spring.kafka.consumer.group-id}"
+    )
+    fun createUser(user: User) {
         userService.createUser(user)
             .hasElement()
             .map {
                 if (it) {
-                    response.statusCode = HttpStatus.OK
                     return@map "User Created successfully"
-                }
-                else {
-                    response.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+                } else {
                     return@map "Something went wrong"
                 }
             }
-            .onErrorResume{
-                response.statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+            .onErrorResume {
                 return@onErrorResume Mono.just("Something went wrong")
             }
+            .block()
+    }
     @GetMapping("/{userId}")
     fun getUserById(@PathVariable userId: String, response: ServerHttpResponse) =
         userService.getUser(userId)
@@ -49,14 +52,18 @@ class UserController (
                 response.statusCode = HttpStatus.NOT_FOUND
                 Mono.empty()
             }
-    @DeleteMapping("/{userId}")
-    fun deleteUser(@PathVariable userId: String, response: ServerHttpResponse) =
+    @KafkaListener(
+        topics = [UserTopics.deleteUserTopic],
+        groupId = "\${spring.kafka.consumer.group-id}"
+    )
+    fun deleteUser(@PathVariable userId: String) {
         userService.deleteUser(userId)
             .onErrorResume {
-                response.statusCode = HttpStatus.NOT_FOUND
                 return@onErrorResume Mono.empty()
             }
             .switchIfEmpty {
                 Mono.just("User not found")
             }
+            .block()
+    }
 }
